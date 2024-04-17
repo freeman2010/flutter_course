257d0c4000682097b21aa8105a87926c14d829d0
freeman2010 <freeom0804@gmail.com>
2024-04-17 09:04:25 -0800

    action

--- lib/main.dart
+++ lib/main.dart
@@ -1,76 +1,190 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(const MyApp());
+/// A text field that also has buttons to select all the text and copy the
+/// selected text to the clipboard.
+class CopyableTextField extends StatefulWidget {
+  const CopyableTextField({super.key, required this.title});
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final String title;
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Retrieve Text Input',
-      home: MyCustomForm(),
-    );
-  }
+  State<CopyableTextField> createState() => _CopyableTextFieldState();
 }
 
-// Define a custom Form widget.
-class MyCustomForm extends StatefulWidget {
-  const MyCustomForm({super.key});
-
-  @override
-  State<MyCustomForm> createState() => _MyCustomFormState();
-}
-
-// Define a corresponding State class.
-// This class holds data related to the Form.
-class _MyCustomFormState extends State<MyCustomForm> {
-  // Create a text controller and use it to retrieve the current value
-  // of the TextField.
-  final myController = TextEditingController();
-
-  @override
-  void initState() {
-    super.initState();
-
-    // Start listening to changes.
-    myController.addListener(_printLatestValue);
-  }
+class _CopyableTextFieldState extends State<CopyableTextField> {
+  late final TextEditingController controller = TextEditingController();
 
   @override
   void dispose() {
-    // Clean up the controller when the widget is removed from the widget tree.
-    // This also removes the _printLatestValue listener.
-    myController.dispose();
+    controller.dispose();
     super.dispose();
-  }
-
-  void _printLatestValue() {
-    final text = myController.text;
-    print('Second text field: $text (${text.characters.length})');
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Retrieve Text Input'),
+    return Actions(
+      dispatcher: LoggingActionDispatcher(),
+      actions: <Type, Action<Intent>>{
+        ClearIntent: ClearAction(controller),
+        CopyIntent: CopyAction(controller),
+        SelectAllIntent: SelectAllAction(controller),
+      },
+      child: Builder(builder: (context) {
+        return Scaffold(
+          body: Center(
+            child: Row(
+              children: <Widget>[
+                const Spacer(),
+                Expanded(
+                  child: TextField(controller: controller),
+                ),
+                IconButton(
+                  icon: const Icon(Icons.copy),
+                  onPressed:
+                      Actions.handler<CopyIntent>(context, const CopyIntent()),
+                ),
+                IconButton(
+                  icon: const Icon(Icons.select_all),
+                  onPressed: Actions.handler<SelectAllIntent>(
+                      context, const SelectAllIntent()),
+                ),
+                const Spacer(),
+              ],
+            ),
+          ),
+        );
+      }),
+    );
+  }
+}
+
+/// A ShortcutManager that logs all keys that it handles.
+class LoggingShortcutManager extends ShortcutManager {
+  @override
+  KeyEventResult handleKeypress(BuildContext context, KeyEvent event) {
+    final KeyEventResult result = super.handleKeypress(context, event);
+    if (result == KeyEventResult.handled) {
+      print('Handled shortcut $event in $context');
+    }
+    return result;
+  }
+}
+
+/// An ActionDispatcher that logs all the actions that it invokes.
+class LoggingActionDispatcher extends ActionDispatcher {
+  @override
+  Object? invokeAction(
+    covariant Action<Intent> action,
+    covariant Intent intent, [
+    BuildContext? context,
+  ]) {
+    print('Action invoked: $action($intent) from $context');
+    super.invokeAction(action, intent, context);
+
+    return null;
+  }
+}
+
+/// An intent that is bound to ClearAction in order to clear its
+/// TextEditingController.
+class ClearIntent extends Intent {
+  const ClearIntent();
+}
+
+/// An action that is bound to ClearIntent that clears its
+/// TextEditingController.
+class ClearAction extends Action<ClearIntent> {
+  ClearAction(this.controller);
+
+  final TextEditingController controller;
+
+  @override
+  Object? invoke(covariant ClearIntent intent) {
+    controller.clear();
+
+    return null;
+  }
+}
+
+/// An intent that is bound to CopyAction to copy from its
+/// TextEditingController.
+class CopyIntent extends Intent {
+  const CopyIntent();
+}
+
+/// An action that is bound to CopyIntent that copies the text in its
+/// TextEditingController to the clipboard.
+class CopyAction extends Action<CopyIntent> {
+  CopyAction(this.controller);
+
+  final TextEditingController controller;
+
+  @override
+  Object? invoke(covariant CopyIntent intent) {
+    final String selectedString = controller.text.substring(
+      controller.selection.baseOffset,
+      controller.selection.extentOffset,
+    );
+    Clipboard.setData(ClipboardData(text: selectedString));
+
+    return null;
+  }
+}
+
+/// An intent that is bound to SelectAllAction to select all the text in its
+/// controller.
+class SelectAllIntent extends Intent {
+  const SelectAllIntent();
+}
+
+/// An action that is bound to SelectAllAction that selects all text in its
+/// TextEditingController.
+class SelectAllAction extends Action<SelectAllIntent> {
+  SelectAllAction(this.controller);
+
+  final TextEditingController controller;
+
+  @override
+  Object? invoke(covariant SelectAllIntent intent) {
+    controller.selection = controller.selection.copyWith(
+      baseOffset: 0,
+      extentOffset: controller.text.length,
+      affinity: controller.selection.affinity,
+    );
+
+    return null;
+  }
+}
+
+/// The top level application class.
+///
+/// Shortcuts defined here are in effect for the whole app,
+/// although different widgets may fulfill them differently.
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  static const String title = 'Shortcuts and Actions Demo';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: title,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            TextField(
-              onChanged: (text) {
-                print('First text field: $text (${text.characters.length})');
-              },
-            ),
-            TextField(
-              controller: myController,
-            ),
-          ],
-        ),
+      home: Shortcuts(
+        shortcuts: <LogicalKeySet, Intent>{
+          LogicalKeySet(LogicalKeyboardKey.escape): const ClearIntent(),
+          LogicalKeySet(LogicalKeyboardKey.control, LogicalKeyboardKey.keyC):
+              const CopyIntent(),
+          LogicalKeySet(LogicalKeyboardKey.control, LogicalKeyboardKey.keyA):
+              const SelectAllIntent(),
+        },
+        child: const CopyableTextField(title: title),
       ),
     );
   }
 }
+
+void main() => runApp(const MyApp());
